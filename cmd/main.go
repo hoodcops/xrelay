a9a1f4c9dd5248654fc79ff9ba5d089b1d2c1372
Edward Pie <hackstockpie@gmail.com>
2019-02-01 11:40:53 +0100

    adds twilio config

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,16 @@ const (
 )
 
 var env = struct {
-	Port        int    `envconfig:"PORT" required:"true"`
-	Environment string `envconfig:"ENVIRONMENT" default:"development"`
-	BrokerDSN   string `envconfig:"BROKER_DSN" required:"true"`
+	Port                      int    `envconfig:"PORT" required:"true"`
+	Environment               string `envconfig:"ENVIRONMENT" default:"development"`
+	BrokerDSN                 string `envconfig:"BROKER_DSN" required:"true"`
+	VerificationWorkers       int    `envconfig:"VERIFICATION_WORKERS" default:"5"`
+	InvitationWorkers         int    `envconfig:"INVITATION_WORKERS" default:"5"`
+	AlertWorkers              int    `envconfig:"ALERT_WORKERS" default:"10"`
+	City                      string `envconfig:"CITY" required:"true"`
+	Locale                    string `envconfig:"LOCALE" default:"en"`
+	TwilioVerificationAPIHost string `envconfig:"TWILIO_VERIFICATION_API_HOST" required:"true"`
+	TwilioVerificationAPIKey  string `envconfig:"TWILIO_VERIFICATION_API_KEY" required:"true"`
 }{}
 
 func init() {
